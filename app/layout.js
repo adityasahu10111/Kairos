8965cb4db3a74e61ad394b6bd5cec5da12e4b6c3
Aditya <adi101197sahu@gmail.com>
2026-01-17 03:58:59 +0530

    Header and clerk authentication

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "@/components/theme-provider";
+import { ConvexClientProvider } from "./ConvexClientProvider";
+import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,11 +21,31 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className="bg-linear-to-br from-gray-950 via-zinc-900 to-stone-900 text-white">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <ClerkProvider>
+       
+          <ConvexClientProvider>
+              <Header />
+              <main className="relative min-h-screen container mx-auto px-4">
+                {/* Background glow effects (behind everything) */}
+                <div className="pointer-events-none absolute inset-0 -z-10 overflow-hidden">
+                  <div className="absolute top-0 left-1/4 w-96 h-96 bg-pink-600/20 rounded-full blur-3xl" />
+                  <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-orange-600/20 rounded-full blur-3xl" />
+                </div>
+
+                {/* Page content (above glow) */}
+                <div className="relative z-10">{children}</div>
+              </main>
+          </ConvexClientProvider> 
+          </ClerkProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
